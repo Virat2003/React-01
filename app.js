493892c26b45d6be48134bbd6a7a72cb05f345b2
Virat2003 <viratrane2616@gmail.com>
2025-11-29 12:41:28 +0530

    01-folder done

---
+++ app.js
@@ -0,0 +1,7 @@
+const a=10
+
+export const name = "virat"
+export const arry = [10,20,30,40]
+export const surname = "rane"
+
+export default a
---
+++ script.js
@@ -0,0 +1,12 @@
+// var h1 = document.createElement("h1")
+
+// h1.innerHTML="hello from js"
+
+// document.body.appendChild(h1)
+
+import a from "./app.js"
+import {name,arry} from "./app.js"
+import {surname} from "./app.js"
+
+
+console.log(a,name,arry,surname);
